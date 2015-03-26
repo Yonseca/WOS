264d9c85d170b7de27b67de145cf72323b51a023
lalotone <lalotone@gmail.com>
2015-03-26 15:04:03 +0000

    adding index.php & general.css

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>WOS</title>
+		<link rel="stylesheet" type="text/css" href="styles/general.css">
+	</head>
+<body>
+	<div id="title">
+		<h1>Working Over System</h1>
+	</div>
+	<div id="menu">
+		Menu
+	</div>
+	<div id="firstContainer">
+	TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	<br>TEXT
+	</div>
+</body>
+</html>
